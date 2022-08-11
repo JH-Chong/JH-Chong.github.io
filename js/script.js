5ae4f494c30c740a4e9af3010b053052e0586eb0
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2022-08-11 15:12:12 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-/*
- * Open the drawer when the menu icon is clicked.
- */
- var menu = document.querySelector('#menu');
- var main = document.querySelector('main');
- var drawer = document.querySelector('#drawer');
+/********************************************************************************/
+/* Navigation
+/********************************************************************************/
+let side_menu_button_open = document.getElementById("side-menu-button");
+let side_menu_button_close = document.getElementById("side-menu-close-button");
+side_menu_button_open.addEventListener("click", ToggleSideMenu);
+side_menu_button_close.addEventListener("click", ToggleSideMenu);
 
- menu.addEventListener('click', (event) => {
- 	drawer.classList.toggle('open');
- 	event.stopPropagation(); /* Prevents propagation of the same event from being called. Propagation means bubbling up to parent elements or capturing down to child elements. The stopPropagation() method allows you to prevent propagation of the current event. */
- })
- main.addEventListener('click', () => {
- 	drawer.classList.remove('open');
- })
+let opened = false
+function ToggleSideMenu()
+{
+    console.log("Clicked");
+    if (opened)
+    {
+        opened = false;
+        document.getElementById("side-menu").style.left = "-15%";
+    }
+    else
+    {
+        opened = true;
+        document.getElementById("side-menu").style.left = "0";
+    }
+}
+
+setInterval(CheckScreenWidth, 20);
+
+function CheckScreenWidth()
+{
+    if (opened && (window.innerWidth > 721))
+    {
+        opened = false;
+        document.getElementById("side-menu").style.left = "-15%";
+    }
+}
